40ac828c95fb9d8b5c32eabdcc7e1409a0b6be80
sabaEdr <sabaedr48@gmail.com>
2025-03-09 21:13:40 +0330

    DB

--- backend/tickets/admin.py
+++ backend/tickets/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Movie, Cinema, Hall, Showtime, Seat, Reservation
 
-# Register your models here.
+admin.site.register(Movie)
+admin.site.register(Cinema)
+admin.site.register(Hall)
+admin.site.register(Showtime)
+admin.site.register(Seat)
+admin.site.register(Reservation)
+
