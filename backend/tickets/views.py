6e4ef0498f68e3fdcb971cd0850ffce5ba3fbc64
sabaEdr <sabaedr48@gmail.com>
2025-03-17 22:17:49 +0330

    movies api with filters by title, genre, cinema

--- backend/tickets/views.py
+++ backend/tickets/views.py
@@ -2,11 +2,22 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from tickets.models import Movie
 from tickets.serializers import MovieSerializer
-from tickets.models import Movie  
+from tickets.models import Movie 
 
+from rest_framework import viewsets
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Movie
+from .serializers import MovieSerializer
+from .filters import MovieFilter
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter)  
+    filterset_class = MovieFilter  
+    ordering_fields = ['release_date', 'title']
+                       
+
 
 
